26fa56d4fac60fd29612106cfd2340d368e1b091
AlbinQestaj <albinqestaj@gmail.com>
2024-10-28 23:02:55 +0100

    writing cache.js where the core logic behind the caching is placed

---
+++ services/cache.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function () {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
+
+  // See if we have a value for key in redis
+  const cacheValue = await client.get(key);
+
+  // If we do, return that
+  if (cacheValue) {
+    console.log(cacheValue);
+  }
+
+  // Otherwise, issue the query and chache the results
+  const result = await exec.apply(this, arguments);
+};
